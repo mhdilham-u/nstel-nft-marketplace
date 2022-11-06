cee2f228151f64c1b90ca9f9f4201b75670fb544
Mhd Ilham U <ilhamushul@gmail.com>
2022-11-06 17:32:33 +0700

    Setup project + create navbar components

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const { src, dest, series, watch } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+// const useref = require("gulp-useref");
+
+// Build Sass Task
+function buildStylesTask() {
+  return src("./assets/sass/**/*.scss")
+    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+    .pipe(dest("./assets/css/"));
+}
+
+// Copy Plugin Task
+// function copyPluginsTask() {
+//   return src("./node_modules/boxicons*/**/*")
+//   .pipe(useref())
+//   .pipe(dest("./assets/plugins/"));
+// }
+
+// Watch Sass Task
+function watchStylesTask() {
+  watch("./assets/sass/**/*.scss", buildStylesTask);
+}
+
+exports.default = series(buildStylesTask, watchStylesTask);
