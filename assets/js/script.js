38ebc2becb790eaead98a388867310157a7711e4
Mhd Ilham U <ilhamushul@gmail.com>
2022-11-12 12:35:10 +0700

    Discover and Help section done! (desktop version)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,37 @@
 // ========== COUNTDOWN LIVE BID! ========== //
 
-// Set the date we're counting down to
-var countDownDate = new Date("Jan 5, 2025 15:37:25").getTime();
+// // Set the date we're counting down to
+// var countDownDate = new Date("Jan 5, 2025 15:37:25").getTime();
 
-// Update the count down every 1 second
-var x = setInterval(function () {
-  // Get today's date and time
-  var now = new Date().getTime();
+// // Update the count down every 1 second
+// var x = setInterval(function () {
+//   // Get today's date and time
+//   var now = new Date().getTime();
 
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
+//   // Find the distance between now and the count down date
+//   var distance = countDownDate - now;
 
-  // Time calculations for days, hours, minutes and seconds
-  //   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+//   // Time calculations for days, hours, minutes and seconds
+//   //   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+//   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+//   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  // Output the result in an element with id="demo"
-  document.getElementById("countdownbid").innerHTML = hours + " h :" + minutes + " m :" + seconds + " s ";
+//   // Output the result in an element with id="demo"
+//   document.getElementById("countdownbid").innerHTML = hours + " h :" + minutes + " m :" + seconds + " s ";
 
-  // If the count down is over, write some text
-  //   if (distance < 0) {
-  //     clearInterval(x);
-  //     document.getElementById("demo").innerHTML = "EXPIRED";
-  //   }
-}, 1000);
+//   // If the count down is over, write some text
+//   //   if (distance < 0) {
+//   //     clearInterval(x);
+//   //     document.getElementById("demo").innerHTML = "EXPIRED";
+//   //   }
+// }, 1000);
+
+$(() => {
+  $(".new-card").slice(0, 4).show();
+
+  $(".btn-load").click((e) => {
+    e.preventDefault;
+    $(".new-card:hidden").slice(0, 4).slideDown();
+  });
+});
